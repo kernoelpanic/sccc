a0995c7f2651d806e4163d9b38d545226b6c1a78
aljosha <ajudmayer@sba-research.org>
2018-12-18 16:00:58 +0100

    Added an optional command line option to docker_run_smartcode.sh to use different local port for binding

--- docker_run_smartcode.sh
+++ docker_run_smartcode.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+if [ -z "${1}" ] && [ -z "${PORT+x}" ];
+then 
+	export PORT=8888
+elif [ ! -z "${1}" ];
+then
+	export PORT="${1}"
+fi
+
 docker run \
-  -p 127.0.0.1:8888:8888 \
+  -p 127.0.0.1:${PORT}:8888 \
   --mount type=bind,source=$(pwd),target=/smartcode \
   --net smartnet \
   --hostname smartcode \
