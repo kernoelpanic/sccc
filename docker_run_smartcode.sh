54d381aee60e34ffff366007dac9882ea991fb07
Lukas Anzinger <lukas@lukasanzinger.at>
2018-12-23 01:39:36 +0100

    Deal with spaces in current working directory.

--- docker_run_smartcode.sh
+++ docker_run_smartcode.sh
@@ -10,7 +10,7 @@ fi
 
 docker run \
   -p 127.0.0.1:${PORT}:8888 \
-  --mount type=bind,source=$(pwd),target=/smartcode \
+  --mount type=bind,source="$(pwd)",target=/smartcode \
   --net smartnet \
   --hostname smartcode \
   --ip 172.18.0.3 \
