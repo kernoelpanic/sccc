3521732c9be44b473fb805de94da7c0a69d7ebed
aljosha <ajudmayer@sba-research.org>
2019-12-09 22:07:48 +0100

    updated warmup exercises

--- exercises/SimpleENS/SimpleENS.sol
+++ exercises/SimpleENS/SimpleENS.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.5.12;
  
 contract SimpleENS {
     struct Record {
@@ -8,7 +8,7 @@ contract SimpleENS {
  
     mapping (string => Record) public records;
     
-    function setDomain(string _domain, string _ipaddr) public {
+    function setDomain(string memory _domain, string memory _ipaddr) public {
 		// Register new domain or update existing mapping if owner
         require( records[_domain].owner == address(0x0) ||	 
   				 records[_domain].owner == msg.sender ); 
@@ -16,12 +16,12 @@ contract SimpleENS {
         records[_domain] = Record(msg.sender, _ipaddr);
     }
     
-    function getDomain(string _domain) view public returns(string) {
+    function getDomain(string memory _domain) view public returns(string memory) {
 		// Get the current mapping of a domain name
         return records[_domain].ipaddr;
     }
     
-    function transferDomain(string _domain, address _to) public {
+    function transferDomain(string memory _domain, address _to) public {
 		// Transfer ownership of domain to another address 
         require( records[_domain].owner == msg.sender );
         records[_domain].owner = _to;
